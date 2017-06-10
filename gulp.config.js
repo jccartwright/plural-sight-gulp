4182526da904ca9bd49389351dd225642e116025
John Cartwright <john.c.cartwright@comcast.net>
2017-06-10 17:19:18 -0600

    completed Code Analysis With JSHint and JSCS module

---
+++ gulp.config.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+    var config = {
+
+        //all the JS to be vetted
+        alljs: [
+            './src/**/*.js',
+             './*.js'
+        ]
+    };
+    
+    return config;
+};
